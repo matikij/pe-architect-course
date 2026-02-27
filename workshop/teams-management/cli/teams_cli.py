b14e39a2882a83406957bd3d03d1ebfc0f1e1201
Mateusz Kijowski <406403+matikij@users.noreply.github.com>
2026-02-27 04:37:27 +0100

    change default CLI URL

--- workshop/teams-management/cli/teams_cli.py
+++ workshop/teams-management/cli/teams_cli.py
@@ -9,7 +9,7 @@ import sys
 import requests
 from typing import Optional
 
-API_BASE_URL = "http://teams-api.127.0.0.1.sslip.io"
+API_BASE_URL = "http://localhost:8080"
 
 class TeamsAPI:
     def __init__(self, base_url: str = API_BASE_URL):
